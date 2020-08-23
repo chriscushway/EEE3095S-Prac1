f796ad524ef5ad61440ae31c737a8cd98fe0936f
chriscushway <chriscushway@gmail.com>
2020-08-23 17:23:59 +0200

    added breakpoint comments

--- main.c
+++ main.c
@@ -4,22 +4,23 @@ int shiftOut(int myDataOut) {
     
     int dataPin = 0;
     int clockPin = 0;
-    
-    for (int i=0; i<8; i++)  {
-        clockPin = 0;
+    for (int i=0; i=8; i++)  {
+        clockPin = 0;//breakpoint here
         if ( myDataOut & (1>>i) ) {
             pinState= 1;
-        }else {
+        }
+        else {
             pinState= 0;
         }
+        
         dataPin = 1;
-        clockPin =  1;
+        clockPin =  1; //breakpoint here 
         dataPin = 0;
     }
-    clockPin = 0;
+        clockPin = 0;
 }
-        
+
 int main(){
     shiftOut(0b10110101);
-    return 0;
+    return 1;
 }
