1e4e7f477e8ff7e7a073c79dc2c9c5f99cb0c922
chriscushway <chriscushway@gmail.com>
2020-08-23 18:13:06 +0200

    fixed all errors in code

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 //a very legitimate and well tested function for shifting out data
 int shiftOut(int myDataOut) {
     int dataPin = 0;
     int clockPin = 0;
 
-    for (int i=0; i<8; i++)  {
+    for (int i=7; i>-1; i--)  { //reverse loop to go from Most significant bit first
         clockPin = 0;//breakpoint here
         dataPin = myDataOut & (1<<i) ? 1 : 0;
         clockPin =  1; //breakpoint here 
@@ -12,6 +13,6 @@ int shiftOut(int myDataOut) {
 }
 
 int main(){
-    shiftOut(0b10110101);
-    return 1;
+    shiftOut(0b11010101);
+    return 0; //must return 0 to signal successful run
 }
