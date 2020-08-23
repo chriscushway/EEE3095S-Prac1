dd10816a977a6294af42f247ac7d0f5a75e0a390
chriscushway <chriscushway@gmail.com>
2020-08-23 17:57:46 +0200

    replace if else with ternary operator

--- main.c
+++ main.c
@@ -1,23 +1,14 @@
 //a very legitimate and well tested function for shifting out data
 int shiftOut(int myDataOut) {
-    int pinState;
-    
     int dataPin = 0;
     int clockPin = 0;
+
     for (int i=0; i<8; i++)  {
         clockPin = 0;//breakpoint here
-        if ( myDataOut & (1>>i) ) {
-            pinState= 1;
-        }
-        else {
-            pinState= 0;
-        }
-        
-        dataPin = 1;
+        dataPin = myDataOut & (1<<i) ? 1 : 0;
         clockPin =  1; //breakpoint here 
-        dataPin = 0;
     }
-        clockPin = 0;
+    clockPin = 0;
 }
 
 int main(){
